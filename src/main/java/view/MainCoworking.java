ca5a6d01ea2ab1d6b2dfbd25fb33e36bb30f0fb0
Stefany Maria <sthefan1morais@gmail.com>
2025-11-22 17:20:52 -0300

    init comit

---
+++ src/main/java/view/MainCoworking.java
@@ -0,0 +1,16 @@
+package main.java.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainCoworking extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        // carrega primeira tela
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
