f642574055a01b5e02cac66ae6257a10d7159e4a
Stefany Maria <sthefan1morais@gmail.com>
2025-11-25 15:53:38 -0300

    init comit

--- src/main/java/service/SistemaService.java
+++ src/main/java/service/SistemaService.java
@@ -5,7 +5,6 @@ import main.java.dao.base.DAOBase;
 public class SistemaService {
 
     public void persistirDados() {
-        // Chama o método do DAO para sincronizar dados com ObjectDB
         DAOBase.persistirDadosGlobais();
     }
 
@@ -14,22 +13,10 @@ public class SistemaService {
         if (digits.isEmpty()) return "0,00";
         if (digits.length() == 1) return "0,0" + digits;
         if (digits.length() == 2) return "0," + digits;
-        String integerPart = digits.substring(0, digits.length() - 2).replaceFirst("^0+", "");  // Remove zeros à esquerda
+        String integerPart = digits.substring(0, digits.length() - 2).replaceFirst("^0+", "");
         if (integerPart.isEmpty()) integerPart = "0";
         String decimalPart = digits.substring(digits.length() - 2);
         return integerPart + "," + decimalPart;
     }
-
-    public String formatarHoras(String input) {
-        String digits = input.replaceAll("\\D", "");
-        if (digits.isEmpty()) return "00:00";
-        if (digits.length() == 1) return "00:0" + digits;
-        if (digits.length() == 2) return "00:" + digits;
-        if (digits.length() == 3) return "0" + digits.charAt(0) + ":" + digits.substring(1);
-        if (digits.length() >= 4) return digits.substring(0, 2) + ":" + digits.substring(2, 4);
-        return "00:00";
-    }
-
-
 
 }
