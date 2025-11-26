c9f4ba1b9b06acecfef5dd9b50a9ad890df223be
Stefany Maria <sthefan1morais@gmail.com>
2025-11-26 20:30:31 -0300

    init comit

--- src/main/java/view/MainCoworking.java
+++ src/main/java/view/MainCoworking.java
@@ -1,120 +1,79 @@
 package main.java.view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
-import main.java.dao.adaptacao.EspacoDAO;
-import main.java.dao.adaptacao.PagamentoDAO;
-import main.java.dao.adaptacao.ReservaDAO;
-import main.java.service.EspacoService;
-import main.java.service.PagamentoService;
-import main.java.service.RelatorioService;
-import main.java.service.ReservaService;
-import main.java.view.controller.*;
-import main.java.service.SistemaService;
+import main.java.dao.adaptacao.*;
+import main.java.service.*;
+import main.java.view.util.GerenciadorDeTelas;
 
 import java.util.Optional;
 
 public class MainCoworking extends Application {
-    private Stage primaryStage;
 
-    // Services globais
+    private Stage primaryStage;
+    private GerenciadorDeTelas gerenciador;
+
     private EspacoService espacoService = new EspacoService(new EspacoDAO());
     private PagamentoService pagamentoService = new PagamentoService(new PagamentoDAO());
     private ReservaService reservaService = new ReservaService(new ReservaDAO(), espacoService, pagamentoService);
     private RelatorioService relatorioService = new RelatorioService(reservaService, pagamentoService, espacoService);
-    private SistemaService sistemaService = new SistemaService();  // Novo service para persistência
+    private SistemaService sistemaService = new SistemaService();
+
+    public void mudarScene(String fxml) {
+        gerenciador.mudarCena(fxml);
+    }
 
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
+
+        gerenciador = new GerenciadorDeTelas(
+                this,
+                espacoService,
+                reservaService,
+                pagamentoService,
+                relatorioService,
+                sistemaService
+        );
+
         primaryStage.setOnCloseRequest(e -> {
             if (!confirmarSaida()) {
-                e.consume();  // Cancela fechamento se não confirmar
+                e.consume();
             }
         });
-        mudarScene("TelaInicial.fxml");
+
+        this.mudarScene("TelaInicial.fxml");
     }
 
-    public void mudarScene(String fxmlFile) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFile));
-            Parent root = loader.load();
-            Object controller = loader.getController();
-
-            // Injete services e referência ao mainApp para todos os controllers
-            if (controller instanceof TelaInicialController) {
-                ((TelaInicialController) controller).setMainApp(this);
-            } else if (controller instanceof MenuInicialController) {
-                ((MenuInicialController) controller).setMainApp(this);
-                ((MenuInicialController) controller).setServices(espacoService, reservaService, relatorioService);
-            } else if (controller instanceof MenuEspacosController) {
-                ((MenuEspacosController) controller).setMainApp(this);
-                ((MenuEspacosController) controller).setEspacoService(espacoService);
-            } else if (controller instanceof CadastroEspacoController) {
-                ((CadastroEspacoController) controller).setMainApp(this);
-                ((CadastroEspacoController) controller).setEspacoService(espacoService);
-                ((CadastroEspacoController) controller).setSistemaService(sistemaService);
-            } else if (controller instanceof EditarEspacosController) {
-                ((EditarEspacosController) controller).setMainApp(this);
-                ((EditarEspacosController) controller).setEspacoService(espacoService);
-                ((EditarEspacosController) controller).setReservaService(reservaService);
-            } else if (controller instanceof EditarEspacoDetalhesController) {
-                ((EditarEspacoDetalhesController) controller).setMainApp(this);
-                ((EditarEspacoDetalhesController) controller).setEspacoService(espacoService);
-                ((EditarEspacoDetalhesController) controller).setSistemaService(sistemaService);
-            } else if (controller instanceof ReservaMenuController) {
-                ((ReservaMenuController) controller).setMainApp(this);
-                ((ReservaMenuController) controller).setReservaService(reservaService);
-            } else if (controller instanceof CriarReservaController) {
-                ((CriarReservaController) controller).setMainApp(this);
-                ((CriarReservaController) controller).setServices(espacoService, reservaService);
-            } else if (controller instanceof DetalhesReservaController) {
-                ((DetalhesReservaController) controller).setMainApp(this);
-                ((DetalhesReservaController) controller).setServices(espacoService, reservaService);
-            } else if (controller instanceof CancelarReservaController) {
-                ((CancelarReservaController) controller).setMainApp(this);
-                ((CancelarReservaController) controller).setReservaService(reservaService);
-            } else if (controller instanceof RelatoriosController) {
-                ((RelatoriosController) controller).setMainApp(this);
-                ((RelatoriosController) controller).setRelatorioService(relatorioService);
-                ((RelatoriosController) controller).setEspacoService(espacoService);
-            } else if (controller instanceof RelatorioPopupController) {
-                ((RelatorioPopupController) controller).setRelatorioService(relatorioService);
-            }
-
-            Scene scene = new Scene(root, 800, 600);  // Aumentado de 600x400 para 800x600
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Sistema Coworking");
-            primaryStage.setResizable(true);  // Permite redimensionar
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-            // Adicione Alert para erro de carregamento se quiser
-        }
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 
     private boolean confirmarSaida() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Deseja salvar os dados antes de sair?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                "Deseja salvar os dados antes de sair?",
+                ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+
         alert.setTitle("Confirmação de Saída");
         alert.setHeaderText("Sair do Sistema");
+
         Optional<ButtonType> result = alert.showAndWait();
+
         if (result.isPresent()) {
             if (result.get() == ButtonType.YES) {
-                // Persistir dados: chama service para sincronizar com ObjectDB
-                sistemaService.persistirDados();  // Respeita MVC
-                return true;  // Sai após salvar
+                sistemaService.persistirDados();
+                return true;
+
             } else if (result.get() == ButtonType.NO) {
-                return true;  // Sai sem salvar
+                return true;
+
             } else {
-                return false;  // Cancela saída
+                return false;
             }
         }
-        return false;  // Cancela por padrão
+        return false;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/view/util/GerenciadorDeTelas.java
@@ -0,0 +1,124 @@
+package main.java.view.util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import main.java.service.*;
+import main.java.view.MainCoworking;
+import main.java.view.controller.*;
+
+public class GerenciadorDeTelas {
+
+    private final MainCoworking mainApp;
+
+    private final EspacoService espacoService;
+    private final ReservaService reservaService;
+    private final PagamentoService pagamentoService;
+    private final RelatorioService relatorioService;
+    private final SistemaService sistemaService;
+
+    public GerenciadorDeTelas(
+            MainCoworking mainApp,
+            EspacoService espacoService,
+            ReservaService reservaService,
+            PagamentoService pagamentoService,
+            RelatorioService relatorioService,
+            SistemaService sistemaService
+    ) {
+        this.mainApp = mainApp;
+        this.espacoService = espacoService;
+        this.reservaService = reservaService;
+        this.pagamentoService = pagamentoService;
+        this.relatorioService = relatorioService;
+        this.sistemaService = sistemaService;
+    }
+
+    public void mudarCena(String fxmlFile) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFile));
+            Parent root = loader.load();
+            Object controller = loader.getController();
+
+            injetarDependencias(controller);
+
+            Scene scene = new Scene(root, 800, 600);
+            mainApp.getPrimaryStage().setScene(scene);
+            mainApp.getPrimaryStage().setTitle("Sistema Coworking");
+            mainApp.getPrimaryStage().setResizable(true);
+            mainApp.getPrimaryStage().show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            mostrarErro(fxmlFile);
+        }
+    }
+
+    private void mostrarErro(String fxmlFile) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erro ao Carregar Tela");
+        alert.setHeaderText("Não foi possível carregar a interface");
+        alert.setContentText("O arquivo " + fxmlFile + " não pôde ser carregado.\n" +
+                "Verifique se o FXML existe e está correto.");
+        alert.showAndWait();
+    }
+
+    /**
+     * Injeção centralizada dos controllers
+     */
+    private void injetarDependencias(Object controller) {
+
+        if (controller instanceof TelaInicialController c) {
+            c.setMainApp(mainApp);
+
+        } else if (controller instanceof MenuInicialController c) {
+            c.setMainApp(mainApp);
+            c.setServices(espacoService, reservaService, relatorioService);
+
+        } else if (controller instanceof MenuEspacosController c) {
+            c.setMainApp(mainApp);
+            c.setEspacoService(espacoService);
+
+        } else if (controller instanceof CadastroEspacoController c) {
+            c.setMainApp(mainApp);
+            c.setEspacoService(espacoService);
+            c.setSistemaService(sistemaService);
+
+        } else if (controller instanceof EditarEspacosController c) {
+            c.setMainApp(mainApp);
+            c.setEspacoService(espacoService);
+            c.setReservaService(reservaService);
+
+        } else if (controller instanceof EditarEspacoDetalhesController c) {
+            c.setMainApp(mainApp);
+            c.setEspacoService(espacoService);
+            c.setSistemaService(sistemaService);
+
+        } else if (controller instanceof ReservaMenuController c) {
+            c.setMainApp(mainApp);
+            c.setReservaService(reservaService);
+
+        } else if (controller instanceof CriarReservaController c) {
+            c.setMainApp(mainApp);
+            c.setServices(espacoService, reservaService);
+
+        } else if (controller instanceof DetalhesReservaController c) {
+            c.setMainApp(mainApp);
+            c.setServices(espacoService, reservaService);
+
+        } else if (controller instanceof CancelarReservaController c) {
+            c.setMainApp(mainApp);
+            c.setReservaService(reservaService);
+
+        } else if (controller instanceof RelatoriosController c) {
+            c.setMainApp(mainApp);
+            c.setRelatorioService(relatorioService);
+            c.setEspacoService(espacoService);
+
+        } else if (controller instanceof RelatorioPopupController c) {
+            c.setRelatorioService(relatorioService);
+        }
+    }
+}
+
